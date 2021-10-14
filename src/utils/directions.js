a120a2f916dbc45e14b496fd052b7e3f89a822be
iren <irenn.dev@gmail.com>
2021-10-14 17:42:53 +0900

    refactor: world bounding error

--- src/utils/directions.js
+++ src/utils/directions.js
@@ -1,7 +1,45 @@
-export const Direction = {
-  Left: "Left",
-  Right: "Right",
-  Up: "Up",
-  Down: "Down",
-  None: "None"
+import { Direction } from "../constants/direction";
+import { TileSize } from "../constants/tile";
+
+export const getOrderedDirections = (filter) => {
+  const directions = [Direction.Up, Direction.Left, Direction.Down, Direction.Right];
+
+  if (!filter) {
+    return directions;
+  }
+
+  return directions.filter(filter);
+};
+
+export const getOppositeDirection = (direction) => {
+switch (direction) {
+  case Direction.Left:
+    return Direction.Right;
+  case Direction.Right:
+    return Direction.Left;
+  case Direction.Up:
+    return Direction.Down;
+  case Direction.Down:
+    return Direction.Up;
+  }
+};
+
+export const getPositionByDirection = (x, y, direction) => {
+  switch (direction) {
+    case Direction.Up: {
+      return { x: x + TileSize.x, y: y - TileSize.y };
+    }
+    case Direction.Down: {
+      return { x: x - TileSize.x, y: y + TileSize.y };
+    }
+    case Direction.Left: {
+      return { x: x - TileSize.x, y: y - TileSize.y };
+    }
+    case Direction.Right: {
+      return { x: x + TileSize.x, y: y - TileSize.y };
+    }
+    default: {
+      break;
+    }
+  }
 };
