417e2b1efd02136750764809e2360b096d44936d
iren <irenn.dev@gmail.com>
2021-10-19 02:12:54 +0900

    feat: add join page with kakao message sending

---
+++ src/components/Modal/EnterRoomModalView.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components";
+
+import ModalView from "../shared/ModalView";
+import ModalContent from "../shared/ModalContent";
+
+const EnterRoomModalView = ({ userName, onChange, onSubmit }) => {
+  return (
+    <ModalView padding={20} width={400} height={300}>
+      <ModalContent>
+        <ModalForm onSubmit={onSubmit}>
+          <input type="text" name="userName" value={userName} onChange={onChange} />
+          <input type="submit" value="Join" />
+        </ModalForm>
+      </ModalContent>
+    </ModalView>
+  );
+};
+
+const ModalForm = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+export default EnterRoomModalView;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="theme-color" content="#000000" />
-    <title>CatchME!</title>
-  </head>
-  <body>
-    <noscript>You need to enable JavaScript to run this app.</noscript>
-    <div id="root"></div>
-    <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
-    <script src="../src/index.js"></script>
-  </body>
-</html>
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import store from "./store";
+import GlobalStyle from "./theme/globalStyle";
+import App from "./components/App";
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <GlobalStyle />
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
